6bc8585ca82307c32c4b0ff01edb4601e04ba57c
Marek Nagy <62247071@cuni.cz>
2020-01-13 12:56:05 +0100

    Add project files.

---
+++ PolynomialCalc/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PolynomialCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
