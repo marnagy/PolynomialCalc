815a7b3383bd00f7ab6d8b3d60586d77ff5ff32c
Marek Nagy <62247071@cuni.cz>
2020-01-13 13:04:02 +0100

    Added constructors on Coeficient struct

---
+++ PolynomialCalc/Coeficient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PolynomialCalc
+{
+    public struct Coeficient
+    {
+        int level;
+        int coeficient;
+        public Coeficient(int level) : this(level, 0)
+        {
+
+        }
+        public Coeficient(int level, int startCoeficient)
+        {
+            this.level = level;
+            coeficient = startCoeficient;
+        }
+    }
+}
